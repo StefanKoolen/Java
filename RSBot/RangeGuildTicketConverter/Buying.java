2b2ae1489dc6e4bcf066cba7848359cb6772083b
Stefan Koolen <kaas.koolen@gmail.com>
2012-11-22 20:00:13 +0100

    Next Update to Buying.java
    I will only support Rune arrows for now, so if there are more than 1020
    archery tickets in your inventory, the bot will click on the Ticket
    Merchant.

--- RSBot/RangeGuildTicketConverter/Buying.java
+++ RSBot/RangeGuildTicketConverter/Buying.java
@@ -1,9 +1,14 @@
 import org.powerbot.core.script.job.state.Node;
 import org.powerbot.game.api.methods.input.Keyboard;
+import org.powerbot.game.api.methods.interactive.NPCs;
+import org.powerbot.game.api.methods.tab.Inventory;
+import org.powerbot.game.api.wrappers.interactive.NPC;
 
 
 public class Buying extends Node {
-
+	
+	private static final int ticketID = 1464;
+	
 	@Override
 	public boolean activate() {
 				
@@ -13,9 +18,14 @@ public class Buying extends Node {
 	@Override
 	public void execute() {
 		
+		if(Inventory.getItem(ticketID).getStackSize() >= 1020){		
+		NPC Merchant = NPCs.getNearest(694);
+			if(Merchant.isIdle() == true){
+				Merchant.click(true);
+			}
 		
-		Keyboard.sendText("Test", true);
-		
+		}
+		else Keyboard.sendText("Minder dan 1020", true);
 
 	}
 
