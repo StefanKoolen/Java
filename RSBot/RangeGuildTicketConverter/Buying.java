c8b056398ebc6acd5d61dc06060f5f12556750f8
Stefan Koolen <kaas.koolen@gmail.com>
2012-11-22 22:22:35 +0100

    Buying.java almost finished
    It is now possible with the Buying class to interact with the Merchant
    and buy the Rune arrow. Unfortunately, the bot spams alot and if the
    amount of tickets is lower 1020, he still tries to buy.

--- RSBot/RangeGuildTicketConverter/Buying.java
+++ RSBot/RangeGuildTicketConverter/Buying.java
@@ -1,32 +1,41 @@
+import org.powerbot.core.script.job.Task;
 import org.powerbot.core.script.job.state.Node;
+import org.powerbot.game.api.methods.Widgets;
 import org.powerbot.game.api.methods.input.Keyboard;
 import org.powerbot.game.api.methods.interactive.NPCs;
 import org.powerbot.game.api.methods.tab.Inventory;
+import org.powerbot.game.api.util.Random;
 import org.powerbot.game.api.wrappers.interactive.NPC;
-
+import org.powerbot.game.api.wrappers.widget.WidgetChild;
 
 public class Buying extends Node {
-	
+
 	private static final int ticketID = 1464;
-	
+
 	@Override
 	public boolean activate() {
-				
+
 		return true;
 	}
 
 	@Override
 	public void execute() {
-		
-		if(Inventory.getItem(ticketID).getStackSize() >= 1020){		
-		NPC Merchant = NPCs.getNearest(694);
-			if(Merchant.isIdle() == true){
-				Merchant.click(true);
-			}
-		
-		}
-		else Keyboard.sendText("Minder dan 1020", true);
 
+		if (Inventory.getItem(ticketID).getStackSize() >= 1020) {
+			NPC Merchant = NPCs.getNearest(694);
+			if (Merchant.isIdle() == true && Merchant != null) {
+				Merchant.click(true);
+				WidgetChild runeArrow = Widgets.get(278, 16).getChild(2);
+
+				while(Inventory.getItem(ticketID).getStackSize() > 7000) {
+					runeArrow.interact("Buy");
+					Task.sleep(Random.nextInt(500, 1100));					
+				}
+			}
+
+		} else {
+			Keyboard.sendText("Minder dan 1020", true);
+		}
 	}
 
 }
